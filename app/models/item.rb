5da03edb788d68a5934b6dbfa48e9fe124527259
Leah Riffell <leah.riffell@gmail.com>
2020-10-21 14:57:47 -0600

    Add single search item method

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,16 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+  
+  def self.single_search(attribute, value)
+    if attribute == 'created_at' || attribute == 'updated_at'
+      Item.find_by("#{attribute} = '%#{value.to_date}%'")
+    elsif value.class == Float || value.class == Integer
+      Item.find_by("#{attribute} = #{value}")
+    elsif Item.find_by("LOWER(#{attribute}) = LOWER('#{value}')") == nil
+      Item.find_by("LOWER(#{attribute}) LIKE LOWER('%#{value}%')")
+    else
+      Item.find_by("LOWER(#{attribute}) = LOWER('#{value}')")
+    end
+  end 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,44 @@ RSpec.describe Item do
     it {should validate_presence_of :description}
     it {should validate_presence_of :unit_price}
   end
+
+  describe 'class methods' do
+    describe 'single_search(attribute, value)' do
+      it 'matches an exact match' do
+        fuzzy_match = create(:item, name: 'Ring Security System')
+        exact_match = create(:item, name: 'ring')
+
+        expect(Item.single_search('name', 'ring')).to eq(exact_match)
+      end
+
+      it 'matches case insensitively' do
+        item = create(:item, name: 'Ring Security System')
+
+        expect(Item.single_search('name', 'ring')).to eq(item)
+      end
+
+      it 'can find partial match' do
+        item = create(:item, name: 'Bring It On soundtrack')
+        expect(Item.single_search('name', 'ring')).to eq(item)
+      end
+
+      it 'can match based on timestamps' do
+        item = create(:item, created_at: '2020-10-31')
+        expect(Item.single_search('created_at', 'October+31')).to eq(item)
+
+        item = create(:item, updated_at: '2021-01-01')
+        expect(Item.single_search('updated_at', '1/1/2021')).to eq(item)
+      end
+
+      it 'can get match based on description' do
+        item = create(:item, description: 'Use the Multi-Color LED Flashlight to cast a sickly green glow over your face while telling a zombie story around a campfire. No campfire? Make a fake one with the orange light!')
+        expect(Item.single_search('description', 'light')).to eq(item)
+      end
+
+      it 'can get match based on unit price' do
+        item = create(:item, unit_price: 1.99)
+        expect(Item.single_search('unit_price', 1.99)).to eq(item)
+      end
+    end
+  end
 end
