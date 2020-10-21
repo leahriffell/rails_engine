9dd8024e6867b83050cd71478c716bdfc18c5b2f
Leah Riffell <leah.riffell@gmail.com>
2020-10-20 20:54:05 -0600

    Add item's merchant endpoint Reorganize controllers and routes:   - Namespace items merchant controller   - Name space merchant items controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,3 @@
 class Api::V1::MerchantsController < ApplicationController
-  def items
-    render json: ItemSerializer.new(Merchant.find(merchant_params[:id]).items)
-  end
 
-  def merchant_params
-    params.permit(:id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update, :destroy]
+      namespace :items do
+        get '/:id/merchants', to: 'merchants#show'
+      end
       
-      get 'merchants/:id/items', to: 'merchants#items'
-
       namespace :merchants do
+        get '/:id/items', to: 'items#index'
         get '/most_revenue', to: 'biz_intel#rank_by_revenue'
         get '/:id/revenue', to: 'biz_intel#merchant_revenue'
       end
+
+      resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
